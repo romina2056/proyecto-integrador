4ac0c8d60d9bd32143cf2091048891cd6b0ac26c
Romy <142067278+romina2056@users.noreply.github.com>
2023-11-20 19:43:44 -0300

    se generaron las routes a admin y a auth

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mainRoutes = require('./src/routes/mainRoutes');
 const shopRoutes = require('./src/routes/shopRoutes');
+const adminRoutes = require('./src/routes/adminRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
 // Middlewarea de configuración 
 
@@ -9,9 +11,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded());
 app.use(express.json());
 
-// routas
+// routes
 app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
+app.use('/admin', adminRoutes);
+app.use('/auth', authRoutes);
 
 
 app.listen(4000, () => console.log("servidor corriendo en http://localhost:4000"));
