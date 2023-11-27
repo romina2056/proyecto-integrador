23a992473b1ce165774eaa72a74b23304fd7accf
Romy <142067278+romina2056@users.noreply.github.com>
2023-11-27 19:28:02 -0300

    se bajo nuevamente todos los controladores

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const methodOverride = require('method-override');
+// const methodOverride = require('method-override');
 const mainRoutes = require('./src/routes/main.routes');
 const shopRoutes = require('./src/routes/shop.routes');
 const adminRoutes = require('./src/routes/admin.routes');
@@ -17,7 +17,7 @@ app.set('view', path.join(__dirname, "./src/view"));
 app.use(express.static('public'));
 app.use(express.urlencoded());
 app.use(express.json());
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
 
 // routes
 
