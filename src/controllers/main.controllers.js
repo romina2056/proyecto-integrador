969984db5f7e75eba2523c2cdf2339e878604ad9
Romy <142067278+romina2056@users.noreply.github.com>
2023-12-06 00:14:56 -0300

    actualizada

--- src/controllers/main.controllers.js
+++ src/controllers/main.controllers.js
@@ -3,10 +3,6 @@ const path = require('path')
 
 const mainControllers = {
 home: (req, res) => {
-    const characters = fs.readFileSync(path.resolve(__dirname, '../data/characters.json'));
-    console.log(JSON.parse(characters));
-    // const [results] = data;
-    // console.log(results);
     res.render('home',{title: "Home"})},
 
 contact:(req, res) => res.send('Route for Contact View'),
