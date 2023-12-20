8238158050f4e561d1247d31a76007f93a9e0266
Romy <142067278+romina2056@users.noreply.github.com>
2023-12-20 18:34:10 -0300

    se actualizo edit del ejs

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -17,18 +17,13 @@ const adminControllers = {
     createItem: (req, res) => res.send('Esta es la ruta para agregar un nuevo item'),
     editView: async (req, res) =>  {
         const itemId = req.params.id;
-        const [item] = await getOne(itemId);
+        const [item] = await getOne({ product_id: itemId });
         res.render(path.resolve(__dirname,'../views/admin/edit.ejs'),{
             title: "EDITAR ITEM",
             item
         });},
     editDate: (req, res) => res.send('esta es la vista para realizar la modificacion'),
     delete: (req, res) => res.send('Esta es la vista para eliminar item'),
-
-    // (req, res) => {
-    //     const { id } = req.params;
-    
-    //     res.send(`Usted quiere eliminar su item: ${id}`)}),
 }
 module.exports = adminControllers
 getAll,
