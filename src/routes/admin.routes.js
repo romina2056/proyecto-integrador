9952e4c57a6a3c2ee0bd382480133133707957b6
Romy <142067278+romina2056@users.noreply.github.com>
2023-12-21 09:07:50 -0300

    guardo configuración de multer

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -6,10 +6,12 @@ const adminControllers = require('../controllers/admin.controllers');
 
 router.get('/', adminControllers.admin);
 router.get('/create', adminControllers.create);
-router.post('/create', upload.array('images', 2), adminControllers.createItem);
+router.post('/create', adminControllers.createItem);
 router.get('/edit/:id', adminControllers.editView);
 router.put('/edit/:id', adminControllers.editDate);
 router.delete('delete/:id', adminControllers.delete);
 
 
 module.exports = router;
+
+// upload.array('images', 2),
