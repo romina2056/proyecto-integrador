43e4e9cdcf1a96b571a935cc5f96ac6e52226f65
Romy <142067278+romina2056@users.noreply.github.com>
2023-12-21 09:42:48 -0300

    se creo el objeto product_schema para guardar los item nuevos

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -14,8 +14,24 @@ const adminControllers = {
             title: "CREAR ITEM",
             data
         });},
-    createItem: (req, res) => res.send('Esta es la ruta para agregar un nuevo item'),
-    editView: async (req, res) =>  {
+    createItem: (req, res) => {
+const product_schema ={
+    product_name: req.body.name,
+    product_description: req.body.descrption,
+    price: req.body.price,
+    stock: req.body.stock,
+    discount: req.body.discount,
+    sku: req.body.sku,
+    dues: req.body.dues,
+    category_id: req.body.category,
+    licence_id: req.body.licence,
+    image_front: req.files[0].originalname,
+    back: req.files[1].originalname
+}
+
+        res.send('Esta es la ruta para agregar un nuevo item')
+    },
+        editView: async (req, res) =>  {
         const itemId = req.params.id;
         const [item] = await getOne({ product_id: itemId });
         res.render(path.resolve(__dirname,'../views/admin/edit.ejs'),{
